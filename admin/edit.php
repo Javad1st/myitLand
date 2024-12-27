114607c7dd8b9be119e6b2d54102a002dd87e2a5
Amir25191 <amirtojar86@gmail.com>
2024-12-27 06:05:05 +0330

    Add files via upload

--- admin/edit.php
+++ admin/edit.php
@@ -14,8 +14,24 @@ if (isset($_POST['sub'])) {
     $caption = $_POST['editor1'];
     $writer = $_POST['writer'];
     $time = $_POST['time'];
-    $image = $_POST['image'];
     $tags = $_POST['tags'];
+
+    // بررسی آپلود فایل جدید
+    if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+        $uploadDir = '../uploads/'; // مسیر ذخیره عکس‌ها
+        $imageName = time() . '_' . basename($_FILES['image']['name']); // تغییر نام فایل برای جلوگیری از تکراری بودن
+        $uploadFile = $uploadDir . $imageName;
+
+        // بررسی و انتقال فایل
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile)) {
+            $imagePath = $imageName; // مسیر ذخیره شده در دیتابیس
+        } else {
+            $imagePath = ''; // در صورت خطا در آپلود، مقدار خالی ذخیره می‌شود
+        }
+    } else {
+        // حفظ تصویر فعلی در صورت انتخاب نشدن تصویر جدید
+        $imagePath = $_POST['current_image'];
+    }
 
     $insert = $conn->prepare("UPDATE blogs SET title=?, caption=?, writer=?, date=?, readtime=?, image=?, tags=? WHERE id=?");
     $insert->bindValue(1, $title);
@@ -23,7 +39,7 @@ if (isset($_POST['sub'])) {
     $insert->bindValue(3, $writer);
     $insert->bindValue(4, $date);
     $insert->bindValue(5, $time);
-    $insert->bindValue(6, $image);
+    $insert->bindValue(6, $imagePath);
     $insert->bindValue(7, $tags);
     $insert->bindValue(8, $id);
     $insert->execute();
@@ -43,42 +59,40 @@ if (isset($_POST['sub'])) {
         input{
             margin: 20px 0px;
         }
-        
     </style>
 </head>
 <body dir="rtl" >
     <div class="container">
     <?php  include 'header.php' ?>
 
-
     <h1>بخش افزودن مقاله</h1>
     <p>طراح سایت :امیر عزیز التجار</p> 
   
-  
     <?php  echo $date ?>
-       <form action="" method="POST">
+       <form action="" method="POST" enctype="multipart/form-data">
         <?php  foreach($blogs as $blog):  ?>
         <input name="title" type="text"  placeholder="موضوع مقاله" class="form-control"   value="<?= $blog['title'];  ?>" >
         <br><br><br><br>
         <script src="//cdn.ckeditor.com/4.16.2/full/ckeditor.js"></script>
 
-<textarea name="editor1" id="editor1"><?= $blog['caption'];  ?></textarea>   <br><br>
-    <script>
-  CKEDITOR.replace('editor1');
-     </script>
+        <textarea name="editor1" id="editor1"><?= $blog['caption'];  ?></textarea>   <br><br>
+        <script>
+            CKEDITOR.replace('editor1');
+        </script>
 
-<input class="form-control mb-5" type="text" name="writer" value="<?= $blog['writer'];  ?>" placeholder="نویسنده">
-      <input  name="time" type="number"  value="<?= $blog['readtime'];  ?>"  placeholder="زمان تقریبی مطالعه" class="form-control mb-5" >
-      <input name="image"  type="text" value="<?= $blog['image'];  ?>"  placeholder="لینک عکس"class="form-control mb-5" >
-      <input name="tags" type="text"  placeholder="تگ ها"class="form-control mb-5" value="<?= $blog['tags'];  ?>" >
-      <input  name="sub" type="submit" class="btn btn-success form-control" value="ثبت ویرایش"  >
-    <?php endforeach;  ?>  
+        <input class="form-control mb-5" type="text" name="writer" value="<?= $blog['writer'];  ?>" placeholder="نویسنده">
+        <input  name="time" type="number"  value="<?= $blog['readtime'];  ?>"  placeholder="زمان تقریبی مطالعه" class="form-control mb-5" >
+        <img src="../uploads/<?= $blog['image'] ?>" alt="تصویر فعلی" style="width: 150px; height: auto; margin-bottom: 10px;">
+        <input type="hidden" name="current_image" value="<?= $blog['image'] ?>"> <!-- ذخیره تصویر فعلی -->
+        <input name="image"  type="file" placeholder="لینک عکس" class="form-control mb-5">
+
+        <input name="tags" type="text"  placeholder="تگ ها" class="form-control mb-5" value="<?= $blog['tags'];  ?>" >
+        <input  name="sub" type="submit" class="btn btn-success form-control" value="ثبت ویرایش"  >
+        <?php endforeach;  ?>  
     </form>
      
     </div>
 </body>
 <script src="menu.js"></script>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 </html>
