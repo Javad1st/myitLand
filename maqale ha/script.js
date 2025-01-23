bbe28b38c2a64496538dd4893a9394a9e30461d2
Javad1st <javadooshakhlari@gmail.com>
2025-01-23 10:33:20 +0330

    cat ux

--- maqale ha/script.js
+++ maqale ha/script.js
@@ -75,3 +75,17 @@ function updateLikeCountInDatabase(index, count) {
     })
     .catch(error => console.error('خطا در ارسال درخواست:', error));
 }
+////////////////
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cats = document.querySelectorAll('.cat1, .cat2, .cat3, .cat4, .cat5');
+
+    cats.forEach(cat => {
+        cat.addEventListener('click', () => {
+            // حذف کلاس 'click' از همه کت‌ها
+            cats.forEach(c => c.classList.remove('click'));
+            // اضافه کردن کلاس 'click' به کت کلیک شده
+            cat.classList.add('click');
+        });
+    });
+});
